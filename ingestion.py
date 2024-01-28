164b450f12a2a1505ab384c135744a0d5114aed8
Iury <iuryhorner@gmail.com>
2024-01-28 00:19:16 -0300

    prune ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,3 +1,4 @@
+# ingestion.py: load NY taxi data into a postgres container
 import pandas as pd
 import sqlalchemy
 import os
@@ -8,15 +9,15 @@ parse_dates = ["lpep_pickup_datetime", "lpep_dropoff_datetime"]
 # The following file was downloaded with a RUN instruction
 # in the "Dockerfile-ingestion" file
 green_df = pd.read_csv("green_2019.csv.gz",
-                 compression="gzip",
-                 iterator=True,
-                 chunksize=100_000, # Read file in chunks of 100_000
-                 parse_dates=parse_dates)
+                       compression="gzip",
+                       iterator=True,
+                       chunksize=100_000, # Read file in chunks of 100_000
+                       parse_dates=parse_dates)
 
 # This is a small file
 zones_df = pd.read_csv("zones.csv")
 
-# Create engine to establish connection with the Postgres database
+# Create engine to establish a connection with the Postgres database
 engine = sqlalchemy.create_engine(f"postgresql://"
                                   f"{os.environ['POSTGRES_USER']}:"
                                   f"{os.environ['POSTGRES_PASSWORD']}@"
@@ -26,37 +27,27 @@ engine = sqlalchemy.create_engine(f"postgresql://"
 
 # Establish the connection to start inserting data
 with engine.connect() as connection:
-    # Create a query to drop table with green taxi data if exists
-    table_name = "green_september_2019"
-    query = f"DROP TABLE IF EXISTS {table_name}"
-    # Execute the drop table statement before writing
-    connection.execute(sqlalchemy.text(query))
-    connection.commit()
-
-    i=0
-    total_rows = 0
+    i, total_rows = 0, 0
     print("-" * 20)
+
     # Write chunks to Postgres
     for chunk in green_df:
         print(f"Iteration {i}:\n")
-        print(f"Writing {chunk.shape[0]} rows to table: {table_name} ...")
-        chunk.to_sql(table_name, con=connection, if_exists="append")
+        print(f"Writing {chunk.shape[0]} rows to table: green_taxi_2019 ...")
+
+        chunk.to_sql("green_taxi_2019", con=connection, if_exists="append")
         connection.commit()
+
         print("-" * 20)
         total_rows += chunk.shape[0]
         i+=1
+    print(f"Succesfully wrote {total_rows} rows to table: green_taxi_2019\n")
 
-    print(f"Succesfully wrote {total_rows} rows to {table_name}")
-
-    # Create a query to drop zones table if exists
-    table_name = "zones"
-    query = f"DROP TABLE IF EXISTS {table_name}"
-    # Execute the drop table statement before writing
-    connection.execute(sqlalchemy.text(query))
-    connection.commit()
+    print("-" * 20)
+    print(f"Writing {zones_df.shape[0]} rows to table: zones")
 
     # Write zones data to Postgres
-    print(f"Writing {zones_df.shape[0]} rows to table: {table_name}")
-    zones_df.to_sql(table_name, con=connection, if_exists="replace")
+    zones_df.to_sql("zones", con=connection, if_exists="replace")
     connection.commit()
-    print(f"Succesfully wrote {zones_df.shape[0]} rows to {table_name}")
+
+    print(f"Succesfully wrote {zones_df.shape[0]} rows to table: zones")
